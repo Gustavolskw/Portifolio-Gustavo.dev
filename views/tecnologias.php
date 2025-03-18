88145e0e37b4638fe971f9ad3a091e0a8a447b6c
Gustavolskw <gustavolschmidt13@gmail.com>
2025-03-18 16:59:16 -0300

    fix: alterações para produção

--- views/tecnologias.php
+++ views/tecnologias.php
@@ -5,7 +5,7 @@
     <?php $index = 0;
     foreach ($techs as $tech): ?>
         <div class="col-md-2 col-4 d-flex justify-content-center">
-            <img class="img-tech icon-tech<?= $index ?>" src="/public/assets/languages/<?= $tech['icon'] ?>"
+            <img class="img-tech icon-tech<?= $index ?>" src="public/assets/languages/<?= $tech['icon'] ?>"
                 alt="icone do(a) <?= $tech['name'] ?>">
         </div>
         <?php
