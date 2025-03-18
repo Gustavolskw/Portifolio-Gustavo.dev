bf62dc621a6775758431265756ff38e6d477763e
Gustavolskw <gustavolschmidt13@gmail.com>
2025-03-18 15:50:24 -0300

    Fix: mudanca na apresentação das tecnologias que domina, para ficarem centralizadas

--- views/tecnologias.php
+++ views/tecnologias.php
@@ -4,7 +4,7 @@
 <div class="row justify-content-center mx-1 gap-5 pt-3">
     <?php $index = 0;
     foreach ($techs as $tech): ?>
-        <div class="col-md-2 col-4">
+        <div class="col-md-2 col-4 d-flex justify-content-center">
             <img class="img-tech icon-tech<?= $index ?>" src="assets/languages/<?= $tech['icon'] ?>"
                 alt="icone do(a) <?= $tech['name'] ?>">
         </div>
